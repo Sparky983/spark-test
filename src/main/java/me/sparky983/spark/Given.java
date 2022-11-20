57c2cc43f985353e742f59c556182e71ad17b015
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-11-21 08:03:44 +1100

    start on given javadocs

--- src/main/java/me/sparky983/spark/Given.java
+++ src/main/java/me/sparky983/spark/Given.java
@@ -4,25 +4,131 @@ import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * A given object.
+ * <p>
+ * In BDD, the given is state of a system before it has been acted upon.
+ * <p>
+ * After the system has been set up, you may act upon it using {@link #when(Function)}, or
+ * {@link #whenDo(Consumer)} (if the action returns void).
+ *
+ * @author Sparky983
+ * @param <T> The type of the given object.
+ * @since 1.0
+ */
 public final class Given<T> {
 
     private final T given;
 
-    public Given(final T given) {
+    private Given(final T given) {
 
         this.given = given;
     }
 
-    public static <T> Given<T> given(final T t) {
+    /**
+     * Creates a new given with the specified given object.
+     * <p>
+     * This method is often statically-imported to make a test more human-readable.
+     * <p>
+     * This method is often statically-imported to make a test more human-readable.
+     * <p>
+     * Example:
+     * <pre>
+     * import static me.sparky983.spark.Given.given;
+     *
+     * given("a string")
+     *         .when(...)
+     *         .then(...);
+     * </pre>
+     * as opposed to
+     * <pre>
+     * import me.sparky983.spark.Given;
+     *
+     * Given.given("a string")
+     *         .when(...)
+     *         .then(...);
+     * </pre>
+     *
+     * @param given the given object.
+     * @return the newly created given.
+     * @param <T> the type of the given object.
+     * @see #givenNull()
+     * @see #givenNull(Class)
+     * @since 1.0
+     */
+    public static <T> Given<T> given(final T given) {
 
-        return new Given<>(t);
+        return new Given<>(given);
     }
 
+    /**
+     * Creates a new given of {@code null}.
+     * <p>
+     * In certain circumstances (when the {@code T} cannot be inferred), it is better to use
+     * {@link #givenNull(Class)}, or {@link #given(Object) given((T) null)} rather than the
+     * lengthier {@code Given.<T>givenNull()}.
+     * <p>
+     * This method is often statically-imported to make a test more human-readable.
+     * <p>
+     * Example:
+     * <pre>
+     * import static me.sparky983.spark.Given.givenNull;
+     *
+     * givenNull()
+     *         .when(...)
+     *         .then(...);
+     * </pre>
+     * as opposed to
+     * <pre>
+     * import me.sparky983.spark.Given;
+     *
+     * Given.givenNull()
+     *         .when(...)
+     *         .then(...);
+     * </pre>
+     *
+     * @return the newly created given.
+     * @param <T> the type of the given object.
+     * @see #given(Object)
+     * @see #givenNull(Class)
+     * @since 1.0
+     */
     public static <T> Given<T> givenNull() {
 
         return new Given<>(null);
     }
 
+    /**
+     * Creates a new given of {@code null}.
+     * <p>
+     * This method is an alternative to {@link #givenNull()}, which lets you avoid doing the
+     * standard (non-static method) call.
+     * <p>
+     * Example:
+     * <pre>
+     * import static me.sparky983.spark.Given.givenNull;
+     *
+     * givenNull(String.class)
+     *         .when(...)
+     *         .then(...);
+     * </pre>
+     * as opposed to
+     * <pre>{@code
+     * import me.sparky983.spark.Given;
+     *
+     * Given.<String>givenNull()
+     *         .when(...)
+     *         .then(...);
+     * }</pre>
+     *
+     * @param cls the type of the given object.
+     * @return the newly created given.
+     * @param <T> the type of the given object.
+     * @throws NullPointerException if the type is {@code null}.
+     * @see #given(Object)
+     * @see #givenNull()
+     * @since 1.0
+     */
     public static <T> Given<T> givenNull(final Class<T> cls) {
 
         Objects.requireNonNull(cls, "cls");

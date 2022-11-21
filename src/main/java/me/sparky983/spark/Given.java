51e423745c172079d09674b2bc742dc056b55085
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-11-21 15:17:44 +1100

    Finish given documentation

--- src/main/java/me/sparky983/spark/Given.java
+++ src/main/java/me/sparky983/spark/Given.java
@@ -3,6 +3,7 @@ package me.sparky983.spark;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * A given object.
@@ -112,7 +113,7 @@ public final class Given<T> {
      *         .when(...)
      *         .then(...);
      * </pre>
-     * as opposed to
+     * can be used instead of
      * <pre>{@code
      * import me.sparky983.spark.Given;
      *
@@ -120,6 +121,9 @@ public final class Given<T> {
      *         .when(...)
      *         .then(...);
      * }</pre>
+     * <p>
+     * Sometimes, we still want to use static imports, but our type is parameterized. In this case,
+     * the preferred method is to use {@code given((T) null)}.
      *
      * @param cls the type of the given object.
      * @return the newly created given.
@@ -135,20 +139,69 @@ public final class Given<T> {
         return new Given<>(null);
     }
 
+    /**
+     * Creates an action (the when-function) that is used to perform assertions on.
+     * <p>
+     * If the method returns void, you should use {@link #whenDo(Consumer)}.
+     *
+     * @param when the when-function
+     * @return a new when that uses the specified when-function.
+     * @param <R> the type of the result of the when-function.
+     * @throws NullPointerException if the when-function is {@code null}.
+     * @see #whenDo(Consumer)
+     * @since 1.0
+     */
     public <R> When<T, R> when(final Function<T, R> when) {
 
         return new When<>(given, when);
     }
 
-    public When<T, Void> whenDo(final Consumer<T> when) {
+    /**
+     * Creates a void action (the when-function) that is used to perform assertions on.
+     * <p>
+     * Examples:
+     * <pre>
+     * class Car {
+     *     boolean isDriving;
+     *     void drive() {
+     *         isDriving = true;
+     *     }
+     *     boolean isDriving() {
+     *         return isDriving;
+     *     }
+     * }
+     *
+     * import static me.sparky983.spark.When;
+     *
+     * given(new Car())
+     *         .whenDo(Car::drive)
+     *         .then(Car::isDriving)
+     * </pre>
+     * @param when the when-function.
+     * @return a new when that uses the specified when-function and where its result is the given
+     * object.
+     * @throws NullPointerException if the when-function is {@code null}.
+     * @see #when(Function)
+     * @since 1.0
+     */
+    public When<T, T> whenDo(final Consumer<T> when) {
 
         Objects.requireNonNull(when, "when");
         return when((t) -> {
             when.accept(t);
-            return null;
+            return t;
         });
     }
 
+    /**
+     * Performs no operation.
+     * <p>
+     * This is essentially the same as {@link When#when(Supplier)}, but states the intentions more
+     * accurately.
+     *
+     * @return a new when that uses the given object as its result.
+     * @since 1.0
+     */
     public When<T, T> noop() {
 
         return new When<>(given, (__) -> given);
